a9441f2cee94f6509417ff709bc9dfb52be014d5
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:57:28 +0900

    Update AddPatient.java

--- FHIRServer/src/main/java/aaa/AddPatient.java
+++ FHIRServer/src/main/java/aaa/AddPatient.java
@@ -32,7 +32,7 @@ public class AddPatient extends HttpServlet {
 		// TODO Auto-generated method stub
 		String[] para = new String[8];
 		for(int i=0;i<8;i++)
-			para[i]=request.getParameter("f"+i);
+			para[i]=request.getParameter("row"+i);
 		for(int i=0;i<8;i++)
 			System.out.println(para[i]);
 		PrintWriter writer = response.getWriter();
