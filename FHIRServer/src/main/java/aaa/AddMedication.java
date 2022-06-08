f893d5c2d85122a6c2cc79bc9af1c4bd75301386
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:57:11 +0900

    Update AddMedication.java

--- FHIRServer/src/main/java/aaa/AddMedication.java
+++ FHIRServer/src/main/java/aaa/AddMedication.java
@@ -31,7 +31,7 @@ public class AddMedication extends HttpServlet {
 		// TODO Auto-generated method stub
 		String[] para = new String[4];
 		for(int i=0;i<para.length;i++)
-			para[i]=request.getParameter("cm"+i);
+			para[i]=request.getParameter("row"+i);
 		for(int i=0;i<para.length;i++)
 			System.out.println(para[i]);
 		try {
