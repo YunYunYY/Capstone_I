15b3e1aea616c7d09120157dbd720af9adb8453a
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:48:23 +0900

    Update UpdatePatient.java

--- FHIRServer/src/main/java/ccc/UpdatePatient.java
+++ FHIRServer/src/main/java/ccc/UpdatePatient.java
@@ -31,13 +31,35 @@ public class UpdatePatient extends HttpServlet {
 		// TODO Auto-generated method stub
 		String[] para = new String[8];
 		for(int i=0;i<8;i++)
-			para[i]=request.getParameter("up"+i);
+			para[i]=request.getParameter("row"+i);
 		for(int i=0;i<8;i++)
 			System.out.println(para[i]);
 		try {
 			DoMySQL(para);
+			writer.println("<html>");
+			writer.println("<header>");
+			writer.println("<meta http-equiv=\"refresh\"content=\"0;url=http://172.18.221.213:8080/FHIRServer/Patient.jsp\">");
+			//writer.println("<meta http-equiv=\"refresh\"content=\"0;url=http://localhost:8080/FHIRServer/Patient.jsp\">");
+			writer.println("</header>");
+			writer.println("<script type=\"text/javascript\">");
+			writer.println("alert(\"Update Success\");");
+			writer.println("</script>");
+			writer.println("<body>");
+			writer.println("</body>");
+			writer.println("</html>");
 		}
 		catch(Exception e) {
+			writer.println("<html>");
+			writer.println("<header>");
+			writer.println("<meta http-equiv=\"refresh\"content=\"0;url=http://172.18.221.213:8080/FHIRServer/Patient.jsp\">");
+			//writer.println("<meta http-equiv=\"refresh\"content=\"0;url=http://localhost:8080/FHIRServer/Patient.jsp\">");
+			writer.println("</header>");
+			writer.println("<script type=\"text/javascript\">");
+			writer.println("alert(\"Update Error\");");
+			writer.println("</script>");
+			writer.println("<body>");
+			writer.println("</body>");
+			writer.println("</html>");
 			System.out.println("Error ");
 			e.printStackTrace();
 		}}
