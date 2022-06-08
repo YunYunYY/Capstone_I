6b4e8cf4031da5efa0aea2fe58838bf432e2b230
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 15:19:09 +0900

    Update ReadPatient.java

--- FHIRServer/src/main/java/bbb/ReadPatient.java
+++ FHIRServer/src/main/java/bbb/ReadPatient.java
@@ -33,7 +33,7 @@ public class ReadPatient extends HttpServlet {
       // TODO Auto-generated method stub
       String[] para = new String[8];
          for (int i=0; i<para.length; i++)
-         para[i] = request.getParameter("row"+i);
+         para[i] = request.getParameter("rp"+i);
          for (int i=0; i<para.length; i++)
          System.out.println(para[i]);
          String s = DoMySQL(para);
