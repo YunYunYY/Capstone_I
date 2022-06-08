3bc8a91d4952506bbdd79f92c9e0c196680d8ce7
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 15:13:32 +0900

    Update DeleteEncounter.java

--- FHIRServer/src/main/java/Delete/DeleteEncounter.java
+++ FHIRServer/src/main/java/Delete/DeleteEncounter.java
@@ -29,7 +29,8 @@ public class DeleteEncounter extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String s = request.getParameter("row0");
+		String s = request.getParameter("de0");
+		PrintWriter writer = response.getWriter();
 		try {
 			DoMySQL(s);
 			writer.println("<html>");
