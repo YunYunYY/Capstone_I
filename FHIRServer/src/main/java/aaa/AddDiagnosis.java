e7e50225cb7bf14438023d3fe251f48c3e5556f7
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:56:48 +0900

    Update AddDiagnosis.java

--- FHIRServer/src/main/java/aaa/AddDiagnosis.java
+++ FHIRServer/src/main/java/aaa/AddDiagnosis.java
@@ -31,7 +31,7 @@ public class AddDiagnosis extends HttpServlet {
 		// TODO Auto-generated method stub
 		String[] para = new String[4];
 		for(int i=0;i<para.length;i++)
-			para[i]=request.getParameter("cd"+i);
+			para[i]=request.getParameter("row"+i);
 		for(int i=0;i<para.length;i++)
 			System.out.println(para[i]);
 		try {
