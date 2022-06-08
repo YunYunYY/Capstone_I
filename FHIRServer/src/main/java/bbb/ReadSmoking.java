0874434ee87f202c9eff5dac3a6339a3865951e8
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:39:50 +0900

    Update ReadSmoking.java

--- FHIRServer/src/main/java/bbb/ReadSmoking.java
+++ FHIRServer/src/main/java/bbb/ReadSmoking.java
@@ -33,7 +33,7 @@ public class ReadSmoking extends HttpServlet {
       // TODO Auto-generated method stub
       String[] para = new String[3];
          for (int i=0; i<para.length; i++)
-         para[i] = request.getParameter("rs"+i);
+         para[i] = request.getParameter("row"+i);
          for (int i=0; i<para.length; i++)
          System.out.println(para[i]);
          String s = DoMySQL(para);
