e4e64c643bd157408162bb6626049fa6f7a13d3f
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 13:51:02 +0900

    Update UpdateSmoking.java

--- FHIRServer/src/main/java/ccc/UpdateSmoking.java
+++ FHIRServer/src/main/java/ccc/UpdateSmoking.java
@@ -31,7 +31,7 @@ public class UpdateSmoking extends HttpServlet {
 		// TODO Auto-generated method stub
 		String[] para = new String[3];
 		for(int i=0;i<para.length;i++)
-			para[i]=request.getParameter("us"+i);
+			para[i]=request.getParameter("row"+i);
 		for(int i=0;i<para.length;i++)
 			System.out.println(para[i]);
 		try {
