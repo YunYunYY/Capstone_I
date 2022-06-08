f98ab435cfa28fd2fefc55442eaa7c7e48f5359f
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 15:18:17 +0900

    Update ReadEncounter.java

--- FHIRServer/src/main/java/bbb/ReadEncounter.java
+++ FHIRServer/src/main/java/bbb/ReadEncounter.java
@@ -33,7 +33,7 @@ public class ReadEncounter extends HttpServlet {
       // TODO Auto-generated method stub
       String[] para = new String[4];
          for (int i=0; i<para.length; i++)
-         para[i] = request.getParameter("row"+i);
+         para[i] = request.getParameter("re"+i);
          for (int i=0; i<para.length; i++)
          System.out.println(para[i]);
          String s = DoMySQL(para);
