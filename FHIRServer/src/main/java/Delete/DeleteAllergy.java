a8091bc47495c56bf1e4c382b37b747bf24e49bf
YunYunYY <76467359+YunYunYY@users.noreply.github.com>
2022-06-08 15:10:36 +0900

    Update DeleteAllergy.java

--- FHIRServer/src/main/java/Delete/DeleteAllergy.java
+++ FHIRServer/src/main/java/Delete/DeleteAllergy.java
@@ -30,6 +30,7 @@ public class DeleteAllergy extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String s = request.getParameter("row0");
+		PrintWriter writer = response.getWriter();
 		try {
 			DoMySQL(s);
 			writer.println("<html>");
